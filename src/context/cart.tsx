652469df5efdf1903a5661bf7695d20b8dfefff8
gbao <gbao250104@gmail.com>
2023-11-28 19:05:05 +0200

    Add cart context

---
+++ src/context/cart.tsx
@@ -0,0 +1,71 @@
+import { IProduct, ICart, ICartItem } from "@/types";
+import { createContext, useState } from "react";
+
+interface CartProviderProps extends React.PropsWithChildren {
+	children: React.JSX.Element;
+}
+
+export const CartContext = createContext<ICart | null>(null);
+
+export function CartProvider({ children }: CartProviderProps) {
+	const [cartItems, setCartItems] = useState<ICartItem[]>([]);
+
+	const addToCart = (item: IProduct) => {
+		const itemInCart = cartItems.find(
+			(cartItem) => cartItem.id === item.id
+		);
+		if (itemInCart) {
+			setCartItems((prevCartItems) =>
+				prevCartItems.map((cartItem) =>
+					cartItem.id === item.id
+						? { ...cartItem, quantity: cartItem.quantity + 1 }
+						: cartItem
+				)
+			);
+		} else {
+			setCartItems((prevCartItems) => [
+				...prevCartItems,
+				{ ...item, quantity: 1 },
+			]);
+		}
+	};
+
+	const updateCart = (product_id: number, quantity: number) => {
+		const newCartItems = [...cartItems];
+		const index = newCartItems.findIndex(
+			(cartItem) => cartItem.id === product_id
+		);
+		if (index !== -1)
+			newCartItems[index] = { ...newCartItems[index], quantity };
+		setCartItems(newCartItems);
+	};
+
+	const removeFromCart = (product_id: number) => {
+		setCartItems((prevCartItems) =>
+			prevCartItems.filter((cartItem) => cartItem.id !== product_id)
+		);
+	};
+
+	const clearCart = () => setCartItems([]);
+
+	const calcCartTotal = () =>
+		cartItems.reduce(
+			(total, curr) => total + curr.price * curr.quantity,
+			0
+		);
+
+	return (
+		<CartContext.Provider
+			value={{
+				cartItems,
+				addToCart,
+				updateCart,
+				removeFromCart,
+				clearCart,
+				calcCartTotal,
+			}}
+		>
+			{children}
+		</CartContext.Provider>
+	);
+}
